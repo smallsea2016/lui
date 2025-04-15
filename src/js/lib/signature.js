7fcea8b3f3b77b35dd21757241057e51bea3b782
黄小海 <wbhuangxh@szlanyou.com>
2025-04-15 20:58:51 +0800

    fix: 修复Signature函数重置画布取值，以及函数参数调整

--- src/js/lib/signature.js
+++ src/js/lib/signature.js
@@ -1,20 +1,26 @@
+
+
 /**
-    * 移动端签名功能
-    * @constructor
-    * @param {string} opts.container 画布外层盒子选择器
-    * @param {string} opts.canvas 画布选择器    
-    * @param {number} opts.ratio 画布高度比列
-    * @param {function} opts.callback 签名绘制完成后的回调，返回base64位图片
-*/
-function Signature(opts) {
+ * 创建签名板对象
+ *
+ * @param {string} el - 画布元素的选择器或元素对象
+ * @param {Object} opts - 配置对象
+ * @param {number} [opts.width=300] - 画布的宽度
+ * @param {number} [opts.height=150] - 画布的高度
+ * @param {Function} [opts.callback] - 签名完成后的回调函数
+ */
+function Signature(el,opts) {
     var _this = this;
     var _opts = opts || {}
-    var ratio = _opts.ratio || 3 / 5;
-    _this.container = $$(_opts.container) || $$('#j_canvas_wrap');
-    _this.canvas = $$(_opts.canvas) || $$('#j_canvas');
+    if(!el || typeof el !== 'string'){
+        throw new Error('请传入画布元素');
+    }
+    _this.canvas = $$(el);
     _this.ctx = _this.canvas.getContext('2d');
-    _this.offsetLeft = _this.container.getBoundingClientRect().left;
-    _this.offsetTop = _this.container.getBoundingClientRect().top;
+    _this.width = _opts.width || 300;
+    _this.height =_opts.height || 150;
+    _this.offsetLeft = _this.canvas.getBoundingClientRect().left;
+    _this.offsetTop = _this.canvas.getBoundingClientRect().top;
     _this.flag = false;
     _this.isTouch = 'ontouchstart' in window;
     _this.img;
@@ -36,8 +42,8 @@ function Signature(opts) {
     (_this.canvasStyle  = function (){
         //画布初始化
         _this.canvas.style.cssText += 'background:url(images/icon/shouxie.png) no-repeat center #fff;background-size:auto 60px'
-        _this.canvas.width = _this.container.clientWidth;
-        _this.canvas.height = _this.container.clientWidth * (ratio);
+        _this.canvas.width = _this.width;
+        _this.canvas.height = _this.height;
     })()
     //签名开始
     _this.canvas.addEventListener(_this.down,
@@ -74,5 +80,8 @@ function Signature(opts) {
     _this.clearRect = function (){
         _this.canvas.style.background = 'url(images/icon/shouxie.png) no-repeat center #fff';
         _this.ctx.clearRect(0,0,_this.canvas.clientWidth,_this.canvas.clientHeight);
+        if (_opts.callback && typeof _opts.callback === "function") {
+            _opts.callback(null);
+          }
     }
 }
